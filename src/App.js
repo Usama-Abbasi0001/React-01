c558c3734c0a988d5ac8ca66ccd7fadcffcf9cbb
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-05-18 06:48:51 +0500

    react-01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
 function App() {
+  const [myDate, setData] = useState([]);
+  //Using Promises
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+      .then((res) => setData(res.data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold text-center mt-10 text-green-800">
+        Fetch API Using Axios
+      </h1>
+      <div className="flex gap-[1px] flex-wrap">
+        {myDate.map((post) => {
+          return (
+            <div className=" rounded-[6px] p-3 border mx-auto mt-10 md:w-1/4 ">
+              <div>Id: {post.id}</div>
+              <div>Name: {post.name}</div>
+              <div>Email: {post.email}</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
